d70475c5f50088e4396807cc43dcdacf1f0942fe
kevin <kevinr060799@gmaail.com>
2024-10-13 17:10:42 -0500

    Se modifica todo el back

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 const app = express();
 const port = 5000;
 
-
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-
+// Simulación de base de datos
 const users = [];
 
-
+// Ruta de registro de usuarios
 app.post('/api/register', (req, res) => {
   const { username, email, password } = req.body;
 
@@ -19,19 +19,19 @@ app.post('/api/register', (req, res) => {
     return res.status(400).json({ message: 'Todos los campos son obligatorios' });
   }
 
- 
+  // Verificar si el usuario ya existe
   const userExists = users.find(user => user.username === username);
   if (userExists) {
     return res.status(400).json({ message: 'El nombre de usuario ya existe' });
   }
 
- 
+  // Guardar el usuario
   users.push({ username, email, password });
-  console.log(users); 
+  console.log(users); // Verificación de que los usuarios se están registrando correctamente
   return res.status(200).json({ message: 'Usuario registrado exitosamente' });
 });
 
-
+// Ruta de inicio de sesión (login)
 app.post('/api/login', (req, res) => {
   const { username, password } = req.body;
 
@@ -39,13 +39,13 @@ app.post('/api/login', (req, res) => {
     return res.status(400).json({ message: 'Username y contraseña son obligatorios' });
   }
 
-
+  // Buscar el usuario por el nombre de usuario
   const user = users.find(user => user.username === username);
   if (!user) {
     return res.status(404).json({ message: 'Usuario no encontrado' });
   }
 
-
+  // Verificar la contraseña
   if (user.password !== password) {
     return res.status(401).json({ message: 'Contraseña incorrecta' });
   }
@@ -53,7 +53,7 @@ app.post('/api/login', (req, res) => {
   return res.status(200).json({ message: 'Inicio de sesión exitoso' });
 });
 
-
+// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
