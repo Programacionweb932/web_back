7acc1b53df271529f79898c3ed258370c1dd3e4a
kevin <kevinr060799@gmaail.com>
2024-11-10 08:46:42 -0500

    se modifica el server.js

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 require('dotenv').config();
+const router = require('./routes');
 
 const app = express();
 
