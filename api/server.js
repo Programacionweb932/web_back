d87deb8e2a19b6a45883131d4b39912646d2ae43
kevin <kevinr060799@gmaail.com>
2024-11-10 09:25:57 -0500

    se modifica el server.js

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const router = require('./routes');  // Asegúrate de que la ruta a routes.js es correcta
+const router = require('../routes');  // Asegúrate de que la ruta a routes.js es correcta
 
 const app = express();
 
