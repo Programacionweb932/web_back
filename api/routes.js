87c1c468cdbec65e3cd4c3e8dec91e67e8ef67c5
kevin <kevinr060799@gmaail.com>
2024-11-11 14:10:18 -0500

    se agrega la api de la creacion del ticket

--- api/routes.js
+++ api/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user'); // Asegúrate de que el modelo 'User' esté bien importado
+const Ticket =require('../models/ticket')
 const userController = require('../controllers/userController');
 const router = express.Router();
 
@@ -14,5 +15,5 @@ router.get('/', (req, res) => {
 router.post('/login', userController.postLogin);
 router.post('/register', userController.postRegistro);
 router.post('/Adminregister', userController.postRegistroAdmin);
-
+router.post ('/ticket',userController.postTicket);
 module.exports = router;
